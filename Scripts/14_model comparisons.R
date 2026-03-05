7630ab957820c99383a5ed84c2fd778731add841
Christian Ching <c.ching@nhm.ac.uk>
2026-03-05 23:08:24 +0000

    Updating GitIgnore

--- Scripts/14_model comparisons.R
+++ Scripts/14_model comparisons.R
@@ -20,13 +20,19 @@ head(global_ds)
 
 str(global_ds)
 
-
+# List data
 datasets <- list(
   single_full      = single_ds,
   single_norain    = single_rl_ds,
   global_full      = global_ds,
   global_norain    = global_rl_ds
 )
+
+globaldatasets <- list(
+  global_full      = global_ds,
+  global_norain    = global_rl_ds
+)
+
 
 ## Data Cleaning Functions: ---------------------------------------------------
 # Create Timerange and Daterange
@@ -49,29 +55,40 @@ prep_dataset <- function(df) {
       TimeHHMMSS = stringr::str_pad(Time_num, width = 6, pad = "0"),
       
       TimeRangeFactor = case_when(
-        TimeHHMMSS >= "050000" & TimeHHMMSS <= "090000" ~ "Dawn",
-        TimeHHMMSS >= "103000" & TimeHHMMSS <= "143000" ~ "Midday",
-        TimeHHMMSS >= "153000" & TimeHHMMSS <= "193000" ~ "Dusk",
-        TimeHHMMSS >= "220000" | TimeHHMMSS <= "020000" ~ "Midnight",
+        TimeHHMMSS >= "040001" & TimeHHMMSS <= "100000" ~ "Dawn",
+        TimeHHMMSS >= "100001" & TimeHHMMSS <= "160000" ~ "Midday",
+        TimeHHMMSS >= "160001" & TimeHHMMSS <= "220000" ~ "Dusk",
+        TimeHHMMSS >= "220001" | TimeHHMMSS <= "040000" ~ "Midnight",
         TRUE ~ "Other"
       ),
       
       TimeRangeFactor = factor(
         TimeRangeFactor,
         levels = c("Dawn","Midday","Dusk","Midnight","Other")
-      )
-    )
+      ),
+      
+      Site = factor(Site),
+      Season = factor(Season),
+      Device = factor(Device)
+    ) %>%
+    
+    # Count devices per Site × Season
+    group_by(Site, Season) %>%
+    mutate(
+      n_devices_site_season = n_distinct(Device)
+    ) %>%
+    ungroup()
 }
 
-datasets <- purrr::map(datasets, prep_dataset)
 
-
-# Function to run model (PC1)
-run_PC1_model4 <- function(dat) {
+# Function to run model (PCA)
+run_PC1_m1 <- function(dat) {
   
   model <- lmer(
-  PC1 ~ QBR * TimeRangeFactor + Strahler +
-      (1 | Site) + (1 | Season),
+    PC1 ~ QBR * TimeRangeFactor +
+      Strahler * TimeRangeFactor +
+      (1 | Site) + 
+      (1 | Season),
     data = dat,
     REML = TRUE
   )
@@ -79,15 +96,64 @@ run_PC1_model4 <- function(dat) {
   return(model)
 }
 
+# Updated Model - nested number of devices within site
+run_PC1_m2 <- function(dat) {
+  
+  if(length(unique(dat$DeployID)) > length(unique(dat$Site))) {
+    
+    model <- lmer(
+      PC1 ~ QBR * TimeRangeFactor +
+        Strahler * TimeRangeFactor +
+        (1 | Site) +
+        (1 | Season) +
+        (1 | DeployID),
+      data = dat,
+      REML = TRUE
+    )
+    
+  } else {
+    
+    model <- lmer(
+      PC1 ~ QBR * TimeRangeFactor +
+        Strahler * TimeRangeFactor +
+        (1 | Site) +
+        (1 | Season),
+      data = dat,
+      REML = TRUE
+    )
+    
+  }
+  
+  return(model)
+}
+
 # # Function to run model (PC2)
-run_PC2_model4 <- function(dat) {
+run_PC2_m1 <- function(dat) {
   
   model <- lmer(
-    PC2 ~ QBR * TimeRangeFactor + Strahler +
-      (1 | Site) + (1 | Season),
+    PC1 ~ QBR * TimeRangeFactor +
+      Strahler * TimeRangeFactor +
+      (1 | Site) + 
+      (1 | Season),
     data = dat,
     REML = TRUE
   )
+  
+  return(model)
+}
+
+# PC2 M2
+run_PC2_m2 <- function(dat) {
+    
+    model <- lmer(
+      PC2 ~ QBR * TimeRangeFactor +
+        Strahler * TimeRangeFactor +
+        (1 | Site) +
+        (1 | Season) +
+        (1 | n_devices_site_season),
+      data = dat,
+      REML = TRUE
+    )
   
   return(model)
 }
@@ -121,398 +187,52 @@ extract_slopes <- function(model, dataset_name) {
       )
   }
 
-# PC1 Analyses: ---------------------------------------------------
-results_all <- purrr::imap_dfr(
+# PC1 Analyses: ---------------------------------------------------#
+
+# Model 1 
+PC1_results_m1 <- purrr::imap_dfr(
   datasets,
-  ~ extract_slopes(run_PC1_model4(.x), .y)
+  ~ extract_slopes(run_PC1_m1(.x), .y)
 )
 
-head(results_all)
+head(PC1_results_m1)
 
-# Build Table (PC1)
-consistency_table <- results_all |>
-  dplyr::group_by(term) |>
-  dplyr::summarise(
-    datasets_tested = n(),
-    positive = sum(direction == "+"),
-    negative = sum(direction == "-"),
-    null = sum(direction == "0"),
-    consistent =
-      (positive == datasets_tested) |
-      (negative == datasets_tested)
-  )
+saveRDS(PC1_results_m1, "clean_data/datasets/PCAmodels/PC1_results_m1.rds")
 
-
-consistency_table 
-
-
-
-## PC1 Heatmap -----------------------------------------------------------------------
-
-# Clean order and order terms
-plot_df <- results_all %>%
-  filter(term != "(Intercept)") %>%
-  mutate(
-    # nicer labels
-    term = str_replace(term, "TimeRangeFactor", ""),
-    term = str_replace(term, "QBR:", "QBR × "),
-    
-    # CI label
-    CI_label = sprintf("%.3f\n[%.3f, %.3f]", estimate, CI_low, CI_high),
-    
-    # colour category
-    effect_dir = case_when(
-      direction == "+" ~ "Positive",
-      direction == "-" ~ "Negative",
-      TRUE ~ "Null"
-    )
-  )
-
-# Order rows 
-term_order <- c(
-  "Midday",
-  "Dusk",
-  "Midnight",
-  "Other",
-  "Strahler",
-  "QBR",
-  "QBR × Midday",
-  "QBR × Dusk",
-  "QBR × Midnight",
-  "QBR × Other"
+# Model 2
+PC1_results_m2 <- purrr::imap_dfr(
+  globaldatasets,
+  ~ extract_slopes(run_PC1_m2(.x), .y)
 )
 
-plot_df <- plot_df %>%
-  mutate(term = factor(term, levels = term_order))
+head(PC1_results_m2)
 
-# Create effect strength metric
-plot_df <- plot_df %>%
-  mutate(
-    # how far the CI is from zero (strength)
-    strength = case_when(
-      direction == "+" ~ CI_low,      # lower bound > 0
-      direction == "-" ~ abs(CI_high),# upper bound < 0
-      TRUE ~ 0
-    )
-  )
+saveRDS(PC1_results_m2, "clean_data/datasets/PCAmodels/PC1_results_m2.rds")
 
-# Order the datasets
-plot_df$dataset <- factor(
-  plot_df$dataset,
-  levels = c("single_full", "single_norain",
-             "global_full", "global_norain")
-)
-
-# Plot v1
-ggplot(plot_df, aes(x = dataset, y = term, fill = effect_dir)) +
-  geom_tile(color = "black") +
-  geom_text(aes(label = CI_label), size = 3) +
-  scale_fill_manual(
-    values = c(
-      "Positive" = "#4CAF50",
-      "Negative" = "#e94f37",
-      "Null"     = "grey80"
-    )
-  ) +
-  labs(
-    x = "Dataset",
-    y = "Model Term",
-    fill = "Effect Direction",
-    title = "Consistency of Mixed Model Effects Across PCA Datasets"
-  ) +
-  theme_minimal(base_size = 13) +
-  theme(
-    panel.grid = element_blank(),
-    axis.text.x = element_text(angle = 45, hjust = 1)
-  )
-
-# Plot  v2
-ggplot(plot_df, aes(x = dataset, y = term, fill = ifelse(direction == "-",
-                                                  -strength,
-                                                  strength))) +
-  geom_tile(color = "black") +
-  geom_text(aes(label = CI_label), size = 3) +
-   scale_fill_gradient2(
-    low = "#e94f37",     # red
-    mid = "grey90",      # null
-    high = "#4CAF50",    # green
-    midpoint = 0,
-    limits = c(
-      -max(plot_df$strength),
-      max(plot_df$strength)
-    ),
-    name = "Effect strength\n(CI distance from 0)"
-  ) +
-  labs(
-    x = "Dataset",
-    y = "Model Term",
-    fill = "Effect Direction",
-    title = "Consistency of Mixed Model Effects Across PCA Datasets"
-  ) +
-  theme_minimal(base_size = 13) +
-  theme(
-    panel.grid = element_blank(),
-    axis.text.x = element_text(angle = 45, hjust = 1)
-  )
-
-
-## PC1 Forest Plot ------------------------
-forest_df <- results_all %>%
-  filter(term != "(Intercept)") %>%
-  mutate(
-    term = gsub("TimeRangeFactor", "", term),
-    term = gsub("QBR:", "QBR × ", term)
-  )
-
-# Ordering
-term_order <- c(
-  "Midday",
-  "Dusk",
-  "Midnight",
-  "Other",
-  "Strahler",
-  "QBR",
-  "QBR × Midday",
-  "QBR × Dusk",
-  "QBR × Midnight",
-  "QBR × Other"
-)
-
-forest_df$term <- factor(forest_df$term, levels = term_order)
-
-forest_df$dataset <- factor(
-  forest_df$dataset,
-  levels = c("single_full", "single_norain",
-             "global_full", "global_norain")
-)
-
-# Plot 
-dodge <- position_dodge(width = 0.6)
-
-ggplot(forest_df,
-       aes(y = term,
-           x = estimate,
-           xmin = CI_low,
-           xmax = CI_high,
-           colour = dataset)) +
-  
-  geom_vline(xintercept = 0, linetype = "dashed") +
-  
-  geom_errorbarh(
-    height = 0.15,
-    size = 0.6,
-    position = dodge
-  ) +
-  
-  geom_point(
-    size = 2.2,
-    position = dodge
-  ) +
-  
-  labs(
-    x = "Effect size (PC1)",
-    y = "Model term",
-    colour = "Dataset",
-    title = "Mixed Model Effect Sizes Across PCA Datasets"
-  ) +
-  
-  theme_minimal(base_size = 13) +
-  theme(
-    panel.grid = element_blank()
-  )
 
 # PC2 Analyses: ---------------------------------------------------
-results_PC2_all <- purrr::imap_dfr(
+PC2_results_m1 <- purrr::imap_dfr(
   datasets,
-  ~ extract_slopes(run_PC2_model4(.x), .y)
+  ~ extract_slopes(run_PC2_m1(.x), .y)
 )
 
-# Build Table (PC2)
-consistency_table_pc2 <- results_PC2_all |>
-  dplyr::group_by(term) |>
-  dplyr::summarise(
-    datasets_tested = n(),
-    positive = sum(direction == "+"),
-    negative = sum(direction == "-"),
-    null = sum(direction == "0"),
-    consistent =
-      (positive == datasets_tested) |
-      (negative == datasets_tested)
-  )
+#Save Results 
+head(PC2_results_m1)
+
+saveRDS(PC2_results_m1, "clean_data/datasets/PCAmodels/PC2_results_m1.rds")
+
+PC2_results_m1 <- readRDS("clean_data/datasets/PCAmodels/PC2_results_m1.rds")
+
+PC2_results_m1
 
 
-consistency_table_pc2 
-
-## PC2 Heatmap -----------------------------------------------------------------------
-
-# Clean order and order terms
-plot_df <- results_all %>%
-  filter(term != "(Intercept)") %>%
-  mutate(
-    # nicer labels
-    term = str_replace(term, "TimeRangeFactor", ""),
-    term = str_replace(term, "QBR:", "QBR × "),
-    
-    # CI label
-    CI_label = sprintf("%.3f\n[%.3f, %.3f]", estimate, CI_low, CI_high),
-    
-    # colour category
-    effect_dir = case_when(
-      direction == "+" ~ "Positive",
-      direction == "-" ~ "Negative",
-      TRUE ~ "Null"
-    )
-  )
-
-# Order rows 
-term_order <- c(
-  "Midday",
-  "Dusk",
-  "Midnight",
-  "Other",
-  "Strahler",
-  "QBR",
-  "QBR × Midday",
-  "QBR × Dusk",
-  "QBR × Midnight",
-  "QBR × Other"
+# Model 2
+PC2_results_m2 <- purrr::imap_dfr(
+  datasets,
+  ~ extract_slopes(run_PC1_m2(.x), .y)
 )
 
-plot_df <- plot_df %>%
-  mutate(term = factor(term, levels = term_order))
+head(PC2_results_m2)
 
-# Create effect strength metric
-plot_df <- plot_df %>%
-  mutate(
-    # how far the CI is from zero (strength)
-    strength = case_when(
-      direction == "+" ~ CI_low,      # lower bound > 0
-      direction == "-" ~ abs(CI_high),# upper bound < 0
-      TRUE ~ 0
-    )
-  )
+saveRDS(PC2_results_m2, "clean_data/datasets/PCAmodels/PC2_results_m2.rds")
 
-# Order the datasets
-plot_df$dataset <- factor(
-  plot_df$dataset,
-  levels = c("single_full", "single_norain",
-             "global_full", "global_norain")
-)
-
-# Plot v1
-ggplot(plot_df, aes(x = dataset, y = term, fill = effect_dir)) +
-  geom_tile(color = "black") +
-  geom_text(aes(label = CI_label), size = 3) +
-  scale_fill_manual(
-    values = c(
-      "Positive" = "#4CAF50",
-      "Negative" = "#e94f37",
-      "Null"     = "grey80"
-    )
-  ) +
-  labs(
-    x = "Dataset",
-    y = "Model Term",
-    fill = "Effect Direction",
-    title = "Consistency of Mixed Model Effects Across PCA Datasets"
-  ) +
-  theme_minimal(base_size = 13) +
-  theme(
-    panel.grid = element_blank(),
-    axis.text.x = element_text(angle = 45, hjust = 1)
-  )
-
-# Plot  v2
-ggplot(plot_df, aes(x = dataset, y = term, fill = ifelse(direction == "-",
-                                                         -strength,
-                                                         strength))) +
-  geom_tile(color = "black") +
-  geom_text(aes(label = CI_label), size = 3) +
-  scale_fill_gradient2(
-    low = "#e94f37",     # red
-    mid = "grey90",      # null
-    high = "#4CAF50",    # green
-    midpoint = 0,
-    limits = c(
-      -max(plot_df$strength),
-      max(plot_df$strength)
-    ),
-    name = "Effect strength\n(CI distance from 0)"
-  ) +
-  labs(
-    x = "Dataset",
-    y = "Model Term",
-    fill = "Effect Direction",
-    title = "Consistency of Mixed Model Effects Across PCA Datasets"
-  ) +
-  theme_minimal(base_size = 13) +
-  theme(
-    panel.grid = element_blank(),
-    axis.text.x = element_text(angle = 45, hjust = 1)
-  )
-
-
-## PC2 Forest Plot ------------------------
-forest_df <- results_all %>%
-  filter(term != "(Intercept)") %>%
-  mutate(
-    term = gsub("TimeRangeFactor", "", term),
-    term = gsub("QBR:", "QBR × ", term)
-  )
-
-# Ordering
-term_order <- c(
-  "Midday",
-  "Dusk",
-  "Midnight",
-  "Other",
-  "Strahler",
-  "QBR",
-  "QBR × Midday",
-  "QBR × Dusk",
-  "QBR × Midnight",
-  "QBR × Other"
-)
-
-forest_df$term <- factor(forest_df$term, levels = term_order)
-
-forest_df$dataset <- factor(
-  forest_df$dataset,
-  levels = c("single_full", "single_norain",
-             "global_full", "global_norain")
-)
-
-# Plot 
-dodge <- position_dodge(width = 0.6)
-
-ggplot(forest_df,
-       aes(y = term,
-           x = estimate,
-           xmin = CI_low,
-           xmax = CI_high,
-           colour = dataset)) +
-  
-  geom_vline(xintercept = 0, linetype = "dashed") +
-  
-  geom_errorbarh(
-    height = 0.15,
-    size = 0.6,
-    position = dodge
-  ) +
-  
-  geom_point(
-    size = 2.2,
-    position = dodge
-  ) +
-  
-  labs(
-    x = "Effect size (PC2)",
-    y = "Model term",
-    colour = "Dataset",
-    title = "Mixed Model Effect Sizes Across PCA Datasets"
-  ) +
-  
-  theme_minimal(base_size = 13) +
-  theme(
-    panel.grid = element_blank()
-  )
